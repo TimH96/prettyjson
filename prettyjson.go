36dd679c3c8b4154cec3b16cb13f29ed1dc61960
TimHasert <timhasert@gmail.com>
2021-06-17 12:34:06 +0200

    check for piped stdin

--- prettyjson.go
+++ prettyjson.go
@@ -3,19 +3,29 @@ prettyjson.go
 author: TimH96
 */
 
+// shell tool to pretty print .json
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
 	info, err := os.Stdin.Stat()
+	// throw piped error
 	if err != nil {
-
+		panic(err)
 	}
-	fmt.Println(os.Args)
+	// panic error when no piped stdin
+	if (info.Mode() & os.ModeNamedPipe) == 0 {
+		panic("No piped input for prettyjson, use prettyjson --help for usage infomration")
+	}
+	// parse json input
+	reader := bufio.NewReader(os.Stdin)
+	//
+	fmt.Println(reader.ReadLine())
 	fmt.Println(info)
 	fmt.Println(err)
 }
